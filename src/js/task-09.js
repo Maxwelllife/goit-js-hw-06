7eeb5f0cf7102115187906b60d6ae948833f76ea
Maksym Pokulytyi <mlightpro@gmail.com>
2022-06-28 17:52:24 +0300

    add 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const button = document.querySelector(".change-color");
+console.log(document.body);
+button.addEventListener("click", onBodyChangeColor);
+
+function onBodyChangeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
 }
